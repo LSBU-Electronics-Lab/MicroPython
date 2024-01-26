7f1bb19822a2156c831e076870e2ad3707277f99
lsbu-electronics <126675512+LSBU-Electronics-Lab@users.noreply.github.com>
2024-01-26 10:50:42 +0000

    Update Distance.py

--- Lab6_Distance_Sensor/Distance.py
+++ Lab6_Distance_Sensor/Distance.py
@@ -1,12 +1,9 @@
 from microbit import *
 import utime
 import time
-from microbit import pin13, pin8, pin12
+from microbit import pin8, pin12
 trigger_pin = pin8
 echo_pin = pin12
-echo_timeout_us = 5
-distance_cm= 100
-
 
 def get_distance():
 
@@ -16,20 +13,12 @@ def get_distance():
     trigger_pin.write_digital(0)
     s = utime.ticks_ms()
     while echo_pin.read_digital() == 0:
-        f = utime.ticks_ms()
-        p = f -s
-        if p > 100:
-            display.show("x")
-            break
-        display.show("a")
-
-        #pass
+         pass
 
     if echo_pin.read_digital()==1:
         start = utime.ticks_us()
         while echo_pin.read_digital() == 1:
-            display.show("b")
-            pass
+              pass
 
         finish = utime.ticks_us()
         pulse_time = finish - start
@@ -39,11 +28,6 @@ def get_distance():
     # 0.034320 cm/us
 
     distance = pulse_time * 0.034 / 2
-    if distance > 500 :
-        distance = 0
-    if distance > 40 :
-        distance = 1
-
     return round(distance)
 
 while(True):
